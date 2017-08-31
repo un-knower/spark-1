07a7841509f3564a0ba0479c6433bcb36031aa05
wangzhiqun <2010zhiqun@gmail.com>
2017-08-31 17:33:56 +0800

    my spark

--- src/main/scala/com/wzq/scala/operation/ParseJson.scala
+++ src/main/scala/com/wzq/scala/operation/ParseJson.scala
@@ -1,7 +1,20 @@
 package com.wzq.scala.operation
 
+import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.sql.SparkSession
+
+/**
+  * json日志处理
+  */
 object ParseJson {
   def main(args: Array[String]): Unit = {
-
+    val conf = new SparkConf().setAppName("json").setMaster("local[2]")
+    val sc=new SparkContext(conf)
+    val ss = SparkSession.builder().getOrCreate()
+    val jrdd=sc.textFile("data/room-20170829.log")
+    val filter=jrdd.filter(_.contains("path"))
+    val df=ss.read.json(filter)
+    df.createOrReplaceTempView("tmp")
+    ss.sql("select * from tmp").show(50)
   }
 }
