29dc0022753595824dc14f1a88396f9985d2ad26
wangzhiqun <2010zhiqun@gmail.com>
2017-09-05 10:15:43 +0800

    my spark

---
+++ src/main/scala/com/wzq/elastic/ReadES.scala
@@ -0,0 +1,22 @@
+package com.wzq.elastic
+
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.{SparkConf, SparkContext}
+
+/**
+  * Created by wangzhiqun on 2017/8/31.
+  */
+object ReadES {
+  def main(args: Array[String]): Unit = {
+    val conf = new SparkConf().setAppName("es").setMaster("local[2]")
+    val sc = new SparkContext(conf)
+    val ss=SparkSession.builder().getOrCreate()
+    val df = ss.read
+      .format("org.elasticsearch.spark.sql")
+      .option("es.nodes", "125.39.176.80")
+      .option("es.port", "9200")
+      .option("path", "data/shakespeare.json")
+      .load()
+    df.show()
+  }
+}
